cf6b8cbc9df6cc19541cbaaacd206a44859d8660
YaroslavAnt <antonchyk.yar@gmail.com>
2018-05-09 02:08:47 +0300

    cleaning

--- src/app/search-pipe.ts
+++ src/app/search-pipe.ts
@@ -8,7 +8,6 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class SearchPipe implements PipeTransform {
     transform(clients, value) {
         return clients.filter(client =>{
-            console.log(typeof client)
             return JSON.stringify(client).toLowerCase().includes(value)
         })
     }
